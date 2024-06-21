098f23baa6ad714a71ed4e82bed88e6a2572c50f
Alexander Bekish <sanyatak3@gmail.com>
2024-06-21 10:56:28 +0300

    Add Task Var, Let, Const Variables
    Переменная var видна после цикла, так как переменные, объявленные через var, имеют функциональную область видимости. Они доступны только в пределах текущей функции или глобального объекта, если функции нет

---
+++ ModuleTask/Variables var, let, const.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 5; i++) {
+    var VarVariable = 'Var Variable';
+    let LetVariable = 'Let Variable';
+    const ConstVariable = 'Const Variable';
+}
+
+console.log(VarVariable)
+//console.log(LetVariable)
+//console.log(ConstVariable)
